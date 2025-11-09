bb85acacc16142ad34332e00ec591c5f7f6ba47b
GAURAV  TAYADE <110455312+GauravTayadeDevloper@users.noreply.github.com>
2025-11-09 14:59:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 // complete the given function
 
 function palindrome(str){
-
+let b ="",j=0;
+	
+	for(let i=str.length-1 ;i>=0;i--)
+		{
+			b[j++]=str[i].toLowerCase();
+		}
+	if(b===str.toLowerCase())
+	{
+		return true;
+	}
+	else{
+		return false;
+	}
 }
 module.exports = palindrome
